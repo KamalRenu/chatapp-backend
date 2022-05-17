1fde2fef625ee07fdc7f0dbe794d4eac4cb36b54
KamalRenu <kamalrenu97@gmail.com>
2022-05-17 19:20:55 +0530

    initial commit

---
+++ routes/messages.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const Message = require('../models/Message.js')
+
+const router = express.Router()
+
+/* Posting a Message based on the chatroom id and senderId */
+
+router.post('/',async (req,res)=>{
+    const newMessage = await new Message({
+        chatroomId:req.body.chatroomId,
+        senderId:req.body.senderId,
+        text:req.body.text
+    })
+    try{
+        const savedMessage = await newMessage.save()
+        res.status(200).json(savedMessage)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+/* Get Messages based on the conversationId */
+
+router.get('/:chatroomId',async(req,res)=>{
+    try{
+        const messages = await Message.find({
+            chatroomId:req.params.chatroomId
+        });
+        res.status(200).json(messages)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
